67618dfafb5b4485760c690a9cc1a8e7cb052391
Igor Sherstuk <igor.sherstuk8229@gmail.com>
2019-03-09 14:52:28 +0200

    Let-const

---
+++ let-const.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//console.log(var1);
+//var var1 = 1;
+const var2 = 2;
+const var3 = 3;
+
+const object1 = { var2, var3 };
+console.log(object1);
+object1.var2 = 4;
+//var3 = 5;
+console.log(object1);
